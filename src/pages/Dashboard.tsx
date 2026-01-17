edec6bd6ac97ec37d8a2a5305e01479123a136ec
Eric Thayer <eric.thayer594@gmail.com>
2026-01-17 10:05:09 -0700

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,7 +20,6 @@ export default function Dashboard() {
           minHeight: { sm: '50dvh' },
           placeContent: 'center',
           py: 'clamp(4rem, 6cqh, 10rem)',
-          px: 'clamp(2rem, 4cqw, 8rem)',
           '@media (25rem < height < 54rem)': {
             height: { '@300': 'calc(100dvh - 65px)' },
           },
