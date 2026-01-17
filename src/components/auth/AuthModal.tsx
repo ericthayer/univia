564f49aad7de02e6c706c47d6f03f643937c70fa
Eric Thayer <eric.thayer594@gmail.com>
2026-01-17 11:00:42 -0700

    Updated AuthModal.tsx

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -105,7 +105,7 @@ export default function AuthModal({ open, onClose, initialMode = 'signin' }: Aut
             </Alert>
           )}
           
-          <Stack gap={3} sx={{ flex: 1 }}>
+          <Stack gap={3} sx={{ width: '100%' }}>
             <Stack>
               {mode === 'signup' && (
                 <TextField
