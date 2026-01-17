4975a4c1c1656b6334c0f3915cfdb574fd2cc13f
Eric Thayer <eric.thayer594@gmail.com>
2026-01-17 10:37:33 -0700

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -241,7 +241,7 @@ export default function Header({ onMobileMenuToggle, mobileMenuOpen = false }: H
               },
             }}
           >
-            <Box component="smmall" sx={{ background: 'primary.main' }}>Menu</Box>
+            <Box component="small" sx={{ background: 'primary.main' }}>Menu</Box>
           </Button>
         </Stack>
 
