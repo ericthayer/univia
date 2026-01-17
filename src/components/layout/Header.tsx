494c2c509d0b80a711f70d178576ab3acb3f96f3
Eric Thayer <eric.thayer594@gmail.com>
2026-01-17 10:41:36 -0700

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -246,7 +246,7 @@ export default function Header({ onMobileMenuToggle, mobileMenuOpen = false }: H
               component="small"
               sx={{
                 bgcolor: 'primary.main',
-                borderRadius '0',
+                borderRadius: 'border',
                 py: 1,
                 px: 2,
               }}
